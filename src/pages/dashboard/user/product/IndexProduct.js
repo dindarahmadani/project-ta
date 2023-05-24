cd909e63328c5e60b7919673c13b71d5ece69b7c
[Dinda Rahmadani] <[dindarahmadani271@gmail.com]>
2023-05-24 22:01:39 +0700

    fix: change folder structure

---
+++ src/pages/dashboard/user/product/IndexProduct.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import UserDashboard from '../../../../components/pageGroup/user/UserDashboard'
+import IndexProduct from '../../../../components/pageGroup/user/product/IndexProduct'
+
+function indexProduct() {
+  return (
+    <>
+    <UserDashboard />
+    <IndexProduct />
+    
+
+    </>
+  )
+}
+
+export default indexProduct
--- src/routes/Indexroutes.js
+++ src/routes/Indexroutes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from '../pages/dashboard/Home';
-import UserProduct from '../pages/dashboard/user/UserProduct';
+import IndexProduct from '../pages/dashboard/user/product/IndexProduct';
 
 function Indexroutes() {
   return (
@@ -9,7 +9,7 @@ function Indexroutes() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>} />
-          <Route path='/userproduct' element={<UserProduct />} />
+          <Route path='/userproduct' element={<IndexProduct />} />
         </Routes>
       </BrowserRouter>
     </>
