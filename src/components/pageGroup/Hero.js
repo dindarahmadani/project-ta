9f7f29dfd0c1d8a2ce5e01e4a4554e087fc3020e
[Dinda Rahmadani] <[dindarahmadani271@gmail.com]>
2023-05-24 14:41:19 +0700

    fix: edit mobile responsive hero

--- src/components/pageGroup/Hero.js
+++ src/components/pageGroup/Hero.js
@@ -3,15 +3,13 @@ import React from 'react'
 function Hero() {
     return (
 
-        <div className='grid grid-cols-2'>
+        <div className='grid grid-cols-1 md:grid-cols-2'>
             <img src='img/hero.jpg' alt='' className='h-250' />
-            <div className='py-32'>
-                <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <div className='p-6 py-2 md:py-32'>
+                <h1 className="text-3xl md:text-5xl font-bold">Box Office News!</h1>
                 <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 <button className="btn btn-warning">Get Started</button>
             </div>
-
-
         </div>
 
     )
