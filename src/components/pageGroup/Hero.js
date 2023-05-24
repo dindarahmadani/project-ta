66ba61b3a8a5e325305504f6540d5df38a7e5db9
[Dinda Rahmadani] <[dindarahmadani271@gmail.com]>
2023-05-24 14:27:51 +0700

    fix: edit hero

--- src/components/pageGroup/Hero.js
+++ src/components/pageGroup/Hero.js
@@ -2,16 +2,18 @@ import React from 'react'
 
 function Hero() {
     return (
-        <div className="hero min-h-screen" style={{ backgroundImage: `url("/img/hero.jpg")` }}>
-            <div className="hero-overlay bg-opacity-0"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
-                </div>
+
+        <div className='grid grid-cols-2'>
+            <img src='img/hero.jpg' alt='' className='h-250' />
+            <div className='py-32'>
+                <h1 className="text-5xl font-bold">Box Office News!</h1>
+                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <button className="btn btn-warning">Get Started</button>
             </div>
+
+
         </div>
+
     )
 }
 
