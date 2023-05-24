1eda02683cfbd468531c09c0517fe2adf9882929
[Dinda Rahmadani] <[dindarahmadani271@gmail.com]>
2023-05-24 21:27:39 +0700

    feat: create ui product

--- src/pages/dashboard/user/UserProduct.js
+++ src/pages/dashboard/user/UserProduct.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import UserDashboard from '../../../components/pageGroup/user/UserDashboard'
+import IndexProduct from '../../../components/pageGroup/user/IndexProduct'
 
 function UserProduct() {
   return (
     <>
     <UserDashboard />
+    <IndexProduct/>
     </>
   )
 }
