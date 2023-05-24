3394df8387e1d54b8b546373737a85db8f8fbe1e
[Dinda Rahmadani] <[dindarahmadani271@gmail.com]>
2023-05-24 15:09:41 +0700

    feat: create user dashboard

---
+++ src/pages/dashboard/user/UserProduct.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserDashboard from '../../../components/pageGroup/user/UserDashboard'
+
+function UserProduct() {
+  return (
+    <>
+    <UserDashboard />
+    </>
+  )
+}
+
+export default UserProduct
--- src/routes/Indexroutes.js
+++ src/routes/Indexroutes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from '../pages/dashboard/Home';
+import UserProduct from '../pages/dashboard/user/UserProduct';
 
 function Indexroutes() {
   return (
@@ -8,7 +9,7 @@ function Indexroutes() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>} />
-
+          <Route path='/userproduct' element={<UserProduct />} />
         </Routes>
       </BrowserRouter>
     </>
